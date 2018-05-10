13ddb5ecb1a1b6a9bad14487d9a67ae11aff8dba
AmiralBl3ndic <camille.briand@efrei.net>
2018-05-10 20:25:24 +0200

    Add `getSymbols()` method to the State class to get all the symbols from its transitions

--- src/State.cpp
+++ src/State.cpp
@@ -4,6 +4,10 @@
 
 #include "State.h"
 
+
+using namespace std;
+
+
 /** @description
  *
  * @param identifier
@@ -12,4 +16,20 @@
  */
 State::State(const std::string &identifier, const bool isInitial, const bool isFinal) : _identifier(identifier), _isInitial(isInitial), _isFinal(isFinal) {
 
+}
+
+/** @description Getter for _symbol attributes of the Transition instances composing the _transitions attribute
+ *
+ *  @return {std::vector<std::string>} The list of all the symbols of the transitions from this state
+ */
+std::vector<std::string> State::getSymbols() const {
+    vector<string> ret;
+    int i(0);
+
+    for (i = 0; i < _transitions.size(); i++) {
+        if (find(ret.begin(), ret.end(), _transitions[i].getSymbol()) != ret.end())
+            ret.push_back(_transitions[i].getSymbol());
+    }
+
+    return ret;
 }
--- src/State.h
+++ src/State.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <algorithm> // So that we can easily find values inside our vectors
 #include "Transition.h"
 
 /** @description This class represents an Automaton's State, it contains a list of transitions (Transition)
@@ -17,6 +18,13 @@ private:
 	
 public:
 	explicit State (const std::string& identifier, bool isInitial = false, bool isFinal = false);
+
+
+	/** @description Getter for _symbol attributes of the Transition instances composing the _transitions attribute
+	 *
+	 *  @return {std::vector<std::string>} The list of all the symbols of the transitions from this state
+	 */
+	std::vector<std::string> getSymbols() const;
 };
 
 
