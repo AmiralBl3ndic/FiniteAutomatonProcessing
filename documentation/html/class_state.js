245951a9f10454672c41486c9acfd209e3a63c9a
AmiralBl3ndic <camille.briand@efrei.net>
2018-05-12 13:34:05 +0200

    Update doxygen documentation

--- documentation/html/class_state.js
+++ documentation/html/class_state.js
@@ -1,6 +1,8 @@
 var class_state =
 [
     [ "State", "class_state.html#a3092d4a73cd0b7cebebb9aa3fec24d0a", null ],
+    [ "addTransition", "class_state.html#a61b58eebbb840c2428c98edb2a9249d9", null ],
+    [ "getIdentifier", "class_state.html#af495568db8f81ebc64c49967c5e0de70", null ],
     [ "getSymbols", "class_state.html#a64f3f5595b34e97b35597d3844648d5e", null ],
     [ "getTransitions", "class_state.html#a0eef598b3f643ee9004c1c56d045bb0f", null ]
 ];
--- documentation/html/search/functions_0.js
+++ documentation/html/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['addtransition',['addTransition',['../class_state.html#a61b58eebbb840c2428c98edb2a9249d9',1,'State']]],
   ['automaton',['Automaton',['../class_automaton.html#ab8d51c2d25cf4dbb440eef63ead2a129',1,'Automaton']]]
 ];
