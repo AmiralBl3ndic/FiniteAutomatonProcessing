78b7bc275a67caf5b62d083ba9f6b7b27cf02aae
AmiralBl3ndic <camille.briand@efrei.net>
2018-05-12 12:47:25 +0200

    Complete documentation for State class' `addTransition()` method

--- src/State.cpp
+++ src/State.cpp
@@ -47,6 +47,7 @@ std::vector<Transition> State::getTransitions() const {
 
 /** @description Part setter for the _transitions attribute: this method allows a `push_back()` to the _transitions attribute
   * @warning Do not use this method if you are not sure the passed symbol is included in the alphabet of the associated Automaton instance
+  * @warning Do not use this method if you are not sure the passed identifier is attributed for the associated Automaton instance
   * @warning To allow a safer use of this method, use the `addTransition()` method of the Automaton class
   *
   * @param symbol The symbol that labels the Transition instance
--- src/State.h
+++ src/State.h
@@ -35,12 +35,13 @@ public:
 
 
     /** @description Part setter for the _transitions attribute: this method allows a `push_back()` to the _transitions attribute
-     *  @warning Do not use this method if you are not sure the passed symbol is included in the alphabet of the associated Automaton instance
-     *  @warning To allow a safer use of this method, use the `addTransition()` method of the Automaton class
-     *
-     *  @param symbol The symbol that labels the Transition instance
-     *  @param identifier The identifier of the arrival State instance
-     */
+      * @warning Do not use this method if you are not sure the passed symbol is included in the alphabet of the associated Automaton instance
+      * @warning Do not use this method if you are not sure the passed identifier is attributed for the associated Automaton instance
+      * @warning To allow a safer use of this method, use the `addTransition()` method of the Automaton class
+      *
+      * @param symbol The symbol that labels the Transition instance
+      * @param identifier The identifier of the arrival State instance
+      */
     bool addTransition(const std::string& symbol, const std::string& identifier);
 };
 
