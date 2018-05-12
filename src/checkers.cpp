64d89fb9b22900f113872062fa6542471f707905
AmiralBl3ndic <camille.briand@efrei.net>
2018-05-12 16:26:15 +0200

    Add a few basic functions as checkers or generators

---
+++ src/checkers.cpp
@@ -0,0 +1,70 @@
+//
+// Created by camboy on 12/05/18.
+//
+
+#include "checkers.h"
+
+using namespace std;
+
+
+/** @description Converts an integer to a std::string using stringstream
+ *
+ *  @return The given integer casted as a string
+ */
+std::string int_to_string(int toConvert) {
+    stringstream stream;
+
+    stream.str(string()); // Empty the stream
+    stream << toConvert;
+    return stream.str();
+}
+
+
+/** @description Generates a vector containing digits from 0 to size (excluded) formatted as strings
+ *
+ *  @param size The size of the vector to generate
+ *  @return A vector containing digits from 0 to size (excluded) in increasing order formatted as strings
+ */
+vector<string> intStringVector(unsigned int size) {
+    int i(0);
+    vector<string> ret;
+
+    for (i = 0; i < size; i++)
+        ret.push_back(int_to_string(i));
+
+    return ret;
+}
+
+
+/** @description Generates a vector containing digits from 0 to 9 formatted as chars
+ *
+ *  @return A vector containing digits from 0 to 9 formatted as chars
+ */
+vector<char> digitsVector() {
+    int i(0);
+    vector<char> ret;
+
+    for (i = 0; i < 10; i++)
+        ret.push_back(int_to_string(i)[0]);
+
+    return ret;
+}
+
+
+
+/** @description Checks if a string is composed only of digits (0 to 9)
+ *
+ *  @return Whether or not the given string is composed only of digits
+ */
+bool isDigit(const string& toCheck) {
+    int i(0);
+    vector<char> digits = digitsVector();
+
+    for (i = 0; i < toCheck.size(); i++)
+        if (find(digits.begin(), digits.end(), toCheck[i]) == digits.end())
+            return false;
+
+    return true;
+}
+
+
---
+++ src/checkers.h
@@ -0,0 +1,45 @@
+//
+// Created by camboy on 12/05/18.
+//
+
+#ifndef TAI_CHECKERS_H
+#define TAI_CHECKERS_H
+
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "constants.h"
+
+
+/** @description Converts an integer to a std::string using stringstream
+ *
+ *  @return The given integer casted as a string
+ */
+std::string int_to_string(int);
+
+
+/** @description Generates a vector containing digits from 0 to size (excluded) formatted as strings
+ *
+ *  @param size The size of the vector to generate
+ *  @return A vector containing digits from 0 to size (excluded) in increasing order formatted as strings
+ */
+std::vector<std::string> intStringVector(unsigned int size);
+
+
+/** @description Generates a vector containing digits from 0 to 9 formatted as chars
+ *
+ *  @return A vector containing digits from 0 to 9 formatted as chars
+ */
+std::vector<char> digitsVector();
+
+
+/** @description Checks if a string is composed only of digits (0 to 9)
+ *
+ *  @return Whether or not the given string is composed only of digits
+ */
+bool isDigit(const std::string&);
+
+
+#endif //TAI_CHECKERS_H
