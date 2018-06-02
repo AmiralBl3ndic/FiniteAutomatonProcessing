df6e3dad7cbb664c73d81d415c58a177db102203
AmiralBl3ndic <camille.briand@efrei.net>
2018-06-02 13:07:24 +0200

    Update documentation

--- documentation/html/search/all_1.js
+++ documentation/html/search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['checkstringalphabet',['checkStringAlphabet',['../class_automaton.html#abca4f921df331df2a4c20199d54eb7b6',1,'Automaton']]]
+  ['checkstringalphabet',['checkStringAlphabet',['../class_automaton.html#abca4f921df331df2a4c20199d54eb7b6',1,'Automaton']]],
+  ['complete',['complete',['../class_automaton.html#a4ed711428be14f85ad4e64199777c377',1,'Automaton']]]
 ];
--- documentation/html/search/all_4.js
+++ documentation/html/search/all_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['isasynchronous',['isAsynchronous',['../class_automaton.html#a5661d627898a3a668b15a5d3507aefe8',1,'Automaton']]],
+  ['iscomplete',['isComplete',['../class_automaton.html#a48ae23af6fc127c46277fdafb09732c7',1,'Automaton']]],
   ['isdeterminist',['isDeterminist',['../class_automaton.html#a27b72847c3a55de5ab8039bef0d66917',1,'Automaton']]],
   ['isstandard',['isStandard',['../class_automaton.html#a11f2853a253808d8bce42543b376ee54',1,'Automaton']]]
 ];
